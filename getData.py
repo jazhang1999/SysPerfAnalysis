25d118b794cfdedcf98d7b0d232c1c8b3bccb538
jazhang1999 <jazhang1999@gmail.com>
2019-07-12 20:36:00 -0400

    Made some changes to these files

--- getData.py
+++ getData.py
@@ -2,12 +2,19 @@
 
 #import necessary libraries
 import requests
+import json
+
 from pprint import pprint as pp
 
-URL = "http://127.0.0.1:8085/data.json"
+URL = "http://192.168.1.18:8085/data.json"
 
 r = requests.get(url = URL)
 
 data = r.json()
 
-pp(data)
+with open('./data.json', 'w') as f:
+    json.dump(data, f)
+
+#pp(data)
+pp("This data has been saved to a local file (or rewritten)");
+
