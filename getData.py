90cb4b6b93aa5421506ed9c0e595d84ad285b2b2
jazhang1999 <jazhang1999@gmail.com>
2019-07-18 21:14:25 -0400

    Added in finishing comments and Doccumentation for this code

--- getData.py
+++ getData.py
@@ -4,6 +4,10 @@
 Pulls out two different types of samples:
 - samples on all of the statistics of the hardware, saved as a .json file
 - the top 20 processes (in terms of cpu usage), saved as a .txt file
+- for now, the user can specify a time limit (in minutes) and a reading will be
+  pulled every 10 seconds (User will be prompted to do so at available time
+- All the data will be dumped into a subdirectory under ./dataFiles for ease
+  of parsing in the future. The user will choose the subdirectory name
 
 All these are pulled periodically, maybe plans to specify frequency and total
 time in the future
@@ -18,6 +22,7 @@ import time
 from pprint import pprint as pp
 from subprocess import check_output
 
+# sys.argv[1] will be the name for which all the pulled data will be stored
 dirname = "./dataFiles/" + sys.argv[1]
 if not os.path.exists(dirname):
         os.mkdir(dirname)
@@ -28,9 +33,10 @@ else:
 # Server where data on hardware is transferred to
 URL = "http://192.168.1.18:8085/data.json"
 
+readingPeriod = int(input('How long should this collection process be? (in minutes): '))
 
-# Sets the total time of reading to be 6 minutes (10 times 60 seconds)
-t_end = time.time() + 60 * 10
+# Sets the total time of reading to be x minutes (x times 60 seconds)
+t_end = time.time() + 60 * readingPeriod
 while time.time() < t_end: 
         timeStamp = time.strftime('%Y-%m-%d-%H:%M:%S')
         
